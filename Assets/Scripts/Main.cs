09baf960634037ee7f73d8c8e875045dba594ed0
eeggoorr1111 <egormywork@gmail.com>
2022-06-02 19:42:12 +0500

    Начальная расстановка объектов на сцене. Заглушки для скриптов. Тестовое передвижение

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,12 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using PingPong.View;
+using PingPong.Model;
 
 namespace PingPong
 {
+    /// <summary>
+    /// Controller в контексте паттерна MVC
+    /// </summary>
     public class Main : MonoBehaviour
     {
-        
+        [Inject]
+        private void Constructor(PingPongView view)
+        {
+            _view = view;
+        }
+
+
+        private PingPongView _view;
+
+
+        private void Awake()
+        {
+            _view.AwakeCustom();
+        }
+        private void Start()
+        {
+            _view.StartCustom();
+        }
+        private void Update()
+        {
+            _view.NextFrame(new Vector2(), new Vector2(), new Vector2());
+        }
     }
 }
 
---
+++ Assets/Scripts/PingPongInstaller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Zenject;
+using PingPong.View;
+
+
+namespace PingPong
+{
+    public class PingPongInstaller : MonoInstaller
+    {
+        [SerializeField] private PingPongView _view;
+
+
+        public override void InstallBindings()
+        {
+            Container.Bind<PingPongView>().FromInstance(_view).AsSingle();
+        }
+    }
+}
